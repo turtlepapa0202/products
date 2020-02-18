3aa5e1a92276e06dafdb73dad6469085eb071a06
Allen Kuo <enlinkuo@gmail.com>
2020-02-18 22:54:24 +0800

    Update write

--- practice_products.py
+++ practice_products.py
@@ -29,7 +29,9 @@ for p in products:
 
 
 # 寫入檔案
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	# f.write('商品' + ',' + '價格' + '\n') # 直接寫法
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
 
